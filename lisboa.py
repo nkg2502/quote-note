16cb2c7c4e66a27d7d0b040bc0bf1866aabd939c
Taesung Jung <tsj.july@gmail.com>
2014-04-24 00:38:26 +0900

    modified first page added index.html closed #1

--- lisboa.py
+++ lisboa.py
@@ -1,13 +1,42 @@
 '''
 	Lisboa prototype
 '''
+import os
+
+from google.appengine.api import users
+
 import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+		loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+		extensions = ['jinja2.ext.autoescape'],
+		autoescape = True)
 
 class MainPage(webapp2.RequestHandler):
 
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Welcome to Lisboa project!')
+		self.response.headers['Content-Type'] = 'text/html'
+
+		user = users.get_current_user()
+
+		nickname = ''
+		log_url = users.create_login_url(self.request.uri)
+		log_msg = 'Login'
+
+		if user:
+			nickname = user.nickname()
+			log_url = users.create_logout_url(self.request.uri)
+			log_msg = 'Logout'
+
+		index_page_value = {
+				'nickname': nickname,
+				'log_url': log_url,
+				'log_msg': log_msg
+		}
+
+		index_page = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(index_page.render(index_page_value))
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
