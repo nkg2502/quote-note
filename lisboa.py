018ead40767afe8d4ce4a7dfe3355fb711041163
Taesung Jung <tsj.july@gmail.com>
2014-04-24 01:07:32 +0900

    added createQuoteBookHandler  * User can create own quotebook added createQuoteBook.html closed #4

--- lisboa.py
+++ lisboa.py
@@ -4,6 +4,7 @@
 import os
 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 import webapp2
 import jinja2
@@ -12,6 +13,11 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 		loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
 		extensions = ['jinja2.ext.autoescape'],
 		autoescape = True)
+
+class QuoteBook(ndb.Model):
+	name = ndb.StringProperty()
+	isbn = ndb.StringProperty()
+	cover_photo = ndb.BlobKeyProperty()
 
 class MainPage(webapp2.RequestHandler):
 
@@ -38,7 +44,32 @@ class MainPage(webapp2.RequestHandler):
 		index_page = JINJA_ENVIRONMENT.get_template('index.html')
 		self.response.write(index_page.render(index_page_value))
 
+class CreateQuoteBookHandler(webapp2.RequestHandler):
+
+	def get(self):
+
+		page = JINJA_ENVIRONMENT.get_template('createQuoteBook.html')
+
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write(page.render())
+
+	def post(self):
+
+		name = self.request.get('name')
+		isbn = self.request.get('isbn')
+
+		user = users.get_current_user()
+
+		if user:
+			quoteBook = QuoteBook(parent=ndb.Key('User', user.email()))
+			quoteBook.name = name
+			quoteBook.isbn = isbn 
+			quoteBook.put()
+
+		self.redirect('/')
+
 application = webapp2.WSGIApplication([
 	('/', MainPage),
+	('/createQuoteBook', CreateQuoteBookHandler),
 ], debug=True)
 
